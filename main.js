2e2128bd8a6f94034313c94f99145f63bc30bf77
Imad Lakouam <lakouam.imad.info@gmail.com>
2025-05-03 12:14:57 +0100

    Update main.js: Show window

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!");
+// import Electron properties
+const {app, BrowserWindow} = require('electron');
+
+
+// function to create a window
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 900, // width of the app
+        height: 600, // hight of the app
+        
+        webPreferences: {
+            // to use node in the render process (page.html)
+            nodeIntegration: true,
+            contextIsolation: false, 
+        }
+    });
+
+
+    // what content that we want to load in this window
+    win.loadFile('page.html');
+
+};
+
+
+// when app is ready open this window "createWindow"
+app.whenReady().then(() => {
+    createWindow();
+});
