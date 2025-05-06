787166a5c626bc3a3ee6149c56cbd6f265095ad8
Imad Lakouam <lakouam.imad.info@gmail.com>
2025-05-06 12:37:35 +0100

    Add Javascript file

---
+++ page.js
@@ -0,0 +1,12 @@
+// always Focus on scanInput execpt when we click on commentInput or postSelect
+{
+    // focus on scanInput when the page loads
+    document.getElementById("scanInput").focus();
+    // focus on scanInput when the page is clicked execpt when we click on commentInput or postSelect
+    document.body.addEventListener("click", function(event) {
+        if (event.target.id !== "commentInput" && event.target.id !== "postSelect") {
+            document.getElementById("scanInput").focus();
+        }
+    });
+}
+
