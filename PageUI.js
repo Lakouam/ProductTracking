a734bfe68e4b7d6018afa36178d1eb1fc21c4336
Imad Lakouam <lakouam.imad.info@gmail.com>
2025-05-22 16:00:12 +0100

    Disable reload & force reload in the app menu, or enable them

--- PageUI.js
+++ PageUI.js
@@ -2,21 +2,26 @@ class PageUI {
 
     static win;
     static contextMenuRightClick;
+    static menu;
 
-    static connect(win, contextMenuRightClick) {
+    static connect(win, contextMenuRightClick, menu) {
         this.win = win;
         this.contextMenuRightClick = contextMenuRightClick;
-
+        this.menu = menu;
     }
 
     // Disable UI
     static disable() {
         // Disable UI (page.html some elements)
         this.win.webContents.send("Disable UI");
-        // disable contextMenuRightClick all items
+        // Disable contextMenuRightClick all items
         this.contextMenuRightClick.items.forEach(item => {
             item.enabled = false;
         });
+
+        // Disable menu third item submenu item 1 (reload) and 2 (force reload)
+        this.menu.items[2].submenu.items[0].enabled = false;
+        this.menu.items[2].submenu.items[1].enabled = false;
     }
 
     // Enable UI
@@ -27,6 +32,10 @@ class PageUI {
         this.contextMenuRightClick.items.forEach(item => {
             item.enabled = true;
         });
+
+        // Enable menu third item submenu item 1 (reload) and 2 (force reload)
+        this.menu.items[2].submenu.items[0].enabled = true;
+        this.menu.items[2].submenu.items[1].enabled = true;
     }
 
 
