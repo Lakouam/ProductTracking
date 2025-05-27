40af598a6e5c1d99969d40ddc3a4afde69d83d6b
Imad Lakouam <lakouam.imad.info@gmail.com>
2025-05-27 09:59:34 +0100

    Disable navigation links

--- DisableUI.js
+++ DisableUI.js
@@ -3,7 +3,7 @@
     function setInputsDisabled(disabled) { // what's elements to disable or enable  
         
         // Select all interactive elements you want to disable
-        const elements = document.querySelectorAll('input, select, .action-card');
+        const elements = document.querySelectorAll('input, select, .action-card, .breadcrumb-link');
         elements.forEach(el => {
             
             if (disabled) {
@@ -28,10 +28,13 @@
 
 
     
-    // Block click events on .action-card when disabled
+    // Block click events on .action-card & .breadcrumb-link when disabled
     document.addEventListener('click', function(e) {
         const card = e.target.closest('.action-card.disabled');
         if (card) e.stopPropagation();
+
+        const navigation = e.target.closest('.breadcrumb-link.disabled');
+        if (navigation) e.stopPropagation();
     }, true);
     
 }
