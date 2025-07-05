4ee9b4048ef41905449b203c831044abe017541d
Imad Lakouam <lakouam.imad.info@gmail.com>
2025-07-05 15:45:39 +0100

    Add an operations page

--- src/code/js/navigation.js
+++ src/code/js/navigation.js
@@ -14,13 +14,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // go to marque.html
 document.addEventListener('DOMContentLoaded', function() {
-    // Find the "Scans" button in the sidebar
+    // Find the "marque" button in the sidebar
     const scansBtn = Array.from(document.querySelectorAll('.main-subtoolbar-btn')).find(item =>
         item.textContent.trim().toLowerCase().includes('marque')
     );
     if (scansBtn) {
         scansBtn.addEventListener('click', function() {
             window.location.href = 'marque.html';
+        });
+    }
+});
+
+
+// go to operations.html
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Scans" button in the sidebar
+    const scansBtn = Array.from(document.querySelectorAll('.main-subtoolbar-btn')).find(item =>
+        item.textContent.trim().toLowerCase().includes('operations')
+    );
+    if (scansBtn) {
+        scansBtn.addEventListener('click', function() {
+            window.location.href = 'operations.html';
         });
     }
 });
