3a6184224918d1c27b5c90325ac9e673f18436d8
Imad Lakouam <lakouam.imad.info@gmail.com>
2025-07-23 08:54:45 +0100

    Solve 'Post N/A' Problem

--- src/code/js/renderprocess/scanner.js
+++ src/code/js/renderprocess/scanner.js
@@ -75,34 +75,36 @@
 {
     // request post actuel from main process then request Table Data columns and row
     ipcRenderer.invoke('Post Actuel').then(postActuel => {
+
+            document.getElementById("scan-product-poste").textContent = postActuel.name;
         
             ipcRenderer.invoke('Table Data', 'scanner', postActuel.name).then(data => {
 
-                document.getElementById("scan-product-nof").textContent = data.rows[0].nof;
-                document.getElementById("scan-product-gamme").textContent = data.rows[0].ref_gamme;
-                document.getElementById("scan-product-operation").textContent = data.rows[0].num_ope;
-                document.getElementById("scan-product-poste").textContent = data.rows[0].poste_machine;
-                document.getElementById("scan-progress-qa").textContent = data.rows[0].qa;
-                document.getElementById("scan-progress-qt").textContent = data.rows[0].qt;
+                if (data.rows[0] !== undefined) {
+                    document.getElementById("scan-product-nof").textContent = data.rows[0].nof;
+                    document.getElementById("scan-product-gamme").textContent = data.rows[0].ref_gamme;
+                    document.getElementById("scan-product-operation").textContent = data.rows[0].num_ope;
+                    //document.getElementById("scan-product-poste").textContent = data.rows[0].poste_machine;
+                    document.getElementById("scan-progress-qa").textContent = data.rows[0].qa;
+                    document.getElementById("scan-progress-qt").textContent = data.rows[0].qt;
 
-                // change the width of the scan-progress-bar-fill based on qa and qt
-                const qa = data.rows[0].qa;
-                const qt = data.rows[0].qt;
-                const progressBarFill = document.querySelector('.scan-progress-bar-fill');
-                if (qt > 0) {
-                    const percentage = (qa / qt) * 100;
-                    progressBarFill.style.width = percentage + '%';
+                    // change the width of the scan-progress-bar-fill based on qa and qt
+                    const qa = data.rows[0].qa;
+                    const qt = data.rows[0].qt;
+                    const progressBarFill = document.querySelector('.scan-progress-bar-fill');
+                    if (qt > 0) {
+                        const percentage = (qa / qt) * 100;
+                        progressBarFill.style.width = percentage + '%';
+                    }
+
+                    // if scan_count is odd, , add class scan-step-value-complet to scanInitialValue and remove class scan-step-value-attente and set the text to "Complet"
+                    if (data.rows[0].scan_count % 2 === 1) {
+                        document.getElementById("scanInitialValue").classList.add("scan-step-value-complet");
+                        document.getElementById("scanInitialValue").classList.remove("scan-step-value-attente");
+                        document.getElementById("scanInitialValue").textContent = "Complet";
+                        document.getElementById("scanInitialCircle").classList.add("scan-step-complet");
+                    } 
                 }
-
-                // if scan_count is odd, , add class scan-step-value-complet to scanInitialValue and remove class scan-step-value-attente and set the text to "Complet"
-                if (data.rows[0].scan_count % 2 === 1) {
-                    document.getElementById("scanInitialValue").classList.add("scan-step-value-complet");
-                    document.getElementById("scanInitialValue").classList.remove("scan-step-value-attente");
-                    document.getElementById("scanInitialValue").textContent = "Complet";
-                    document.getElementById("scanInitialCircle").classList.add("scan-step-complet");
-                } 
-
-                
                     
             });
         });
