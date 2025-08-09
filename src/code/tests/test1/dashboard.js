89210ad1703f772119627ea0ef256f6584aa97b4
Imad Lakouam <lakouam.imad.info@gmail.com>
2025-08-09 12:18:05 +0100

    Remove previous tests.

--- src/code/tests/test1/dashboard.js
+++
@@ -1,8 +0,0 @@
-// send a message to the main process when the user clicks on the card to open the card window
-{
-    document.getElementById('showDataCard').onclick = () => ipcRenderer.send('open-show-data');
-    document.getElementById('modifyNofCard').onclick = () => ipcRenderer.send('open-modify-nof');
-    document.getElementById('modifyPostsCard').onclick = () => ipcRenderer.send('open-modify-posts');
-    document.getElementById('showGammesCard').onclick = () => ipcRenderer.send('open-show-gammes');
-    document.getElementById('showOperationsCard').onclick = () => ipcRenderer.send('open-show-operations');
-}
